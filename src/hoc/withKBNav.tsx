d5c6a8a6e00412246cbb56d8d8e5f65ab7b30cc0
Kartik Maurya <kartikmaurya@Kartiks-MacBook-Air.local>
2023-09-30 21:42:00 +0530

    firt

---
+++ src/hoc/withKBNav.tsx
@@ -0,0 +1,5 @@
+
+
+const withKBNav = ({children}: {children: ReactNode}) => {
+
+}
---
+++ src/hooks/undo-redo/index.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+export const useUndoRedo = ({onUndo, onRedo}: {onUndo: () => void;onRedo:  () => void}) => {
+    useEffect(() => {
+        const onPress = (e) =>{
+          if(!e.metaKey)
+            return;
+          if(e.code !== 'KeyZ')
+            return;
+          if(e.shiftKey)
+          {
+            onRedo();
+            return;
+          }
+          onUndo();
+          return;
+        };
+        document.addEventListener('keydown', onPress)
+        return () => {
+          document.removeEventListener('keydown', onPress)
+        }
+      }, [onUndo, onRedo])
+}
